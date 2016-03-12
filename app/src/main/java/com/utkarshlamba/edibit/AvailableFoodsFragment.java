98a210b555b6c381bf9d3addc730e4553e792ce8
utk9 <utkarshlamba.2@gmail.com>
2016-03-12 16:11:23 -0500

    added details fragment for food items

--- app/src/main/java/com/utkarshlamba/edibit/AvailableFoodsFragment.java
+++ app/src/main/java/com/utkarshlamba/edibit/AvailableFoodsFragment.java
@@ -1,12 +1,14 @@
 package com.utkarshlamba.edibit;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,6 +20,7 @@ import java.util.ArrayList;
 public class AvailableFoodsFragment extends Fragment {
 
     public static CustomListAdapter adapter;
+    public static ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class AvailableFoodsFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ListView listView = (ListView) getActivity().findViewById(R.id.available_foods_listView);
+        listView = (ListView) getActivity().findViewById(R.id.available_foods_listView);
 
         ArrayList list = new ArrayList<String>();
         for (int i = 0; i<Application.foodItemsList.size(); i++){
@@ -38,6 +41,14 @@ public class AvailableFoodsFragment extends Fragment {
 
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int i, long l) {
+                FragmentManager fm = getActivity().getFragmentManager();
+                fm.beginTransaction().replace(R.id.content_frame,
+                        new FoodDetailsFragment(i)).commit();
+            }
+        });
 
         new FetchAvailableFoodsFromDBTask().execute();
 
