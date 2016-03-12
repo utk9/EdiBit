7f0b740c7c5b374881fb644aeeb41547aeef511b
utk9 <utkarshlamba.2@gmail.com>
2016-03-12 01:23:54 -0500

    Added drawerLayout structure

--- app/src/main/java/com/utkarshlamba/edibit/MainActivity.java
+++ app/src/main/java/com/utkarshlamba/edibit/MainActivity.java
@@ -1,15 +1,51 @@
 package com.utkarshlamba.edibit;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ListView drawerList;
+    ActionBarDrawerToggle drawerToggle;
+    ProgressDialog pd;
+    TextView toolbarTitle;
+
+    static ArrayList<String> questionsList;
+    static ArrayList<String> answersList;
+    //static QuestionListAdapter adapter;
+    static ArrayList<Integer> countList;
+
+    public static String smsBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +53,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        pd = new ProgressDialog(this);
+
+        String[] drawerListOptions = getResources().getStringArray(R.array.list_options_array);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+        ListArrayAdapter drawerAdapter = new ListArrayAdapter(this, drawerListOptions);
+
+
+        drawerList.setAdapter(drawerAdapter);
+        drawerList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        /*
+        final FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame, new SearchItemFragment(pd)).commit();
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Fragment fragment = null;
+                if (position == 0) {
+                    fragment = new SearchItemFragment(pd);
+                    toolbarTitle.setText("Wikipedia");
+                } else if (position == 1) {
+                    fragment = new WolframAlphaSearchItemFragment(pd);
+                    toolbarTitle.setText("Wolfram Alpha");
+                } else if (position == 2) {
+
+                    fragment = new AskQuestionFragment(pd);
+                    toolbarTitle.setText("Ask Question");
+                } else {
+                    if (isNetworkAvailable()) {
+                        fragment = new FAQFragment();
+                        toolbarTitle.setText("Helpful Questions");
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "This feature is only available with an network connection",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+                if (fragment != null) {
+                    fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                }
+
+                drawerLayout.closeDrawer(drawerList);
             }
         });
+*/
+        Button toggleButton = (Button) findViewById(R.id.toggle_button);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(Gravity.LEFT)){
+                    drawerLayout.closeDrawer(drawerList);
+
+                }
+                else{
+                    drawerLayout.openDrawer(Gravity.LEFT);
+                }
+
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -43,9 +138,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*
+        if (id == R.id.action_info) {
+            final FragmentManager fm = getFragmentManager();
+            Fragment fragment = new InfoFragment();
+            fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
